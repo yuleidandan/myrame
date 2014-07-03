0a33e1bfa4a33dbe4159589d9a32a89719eead91
余雷 <@yu>
2014-07-03 17:26:53 +0800

    初始化包结构

---
+++ Myframe/src/com/dandan/myframe/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.dandan.myframe.app;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		
+		
+		super.onCreate();
+	}
+}
